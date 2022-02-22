4ad4e95899af1c5f6580ca0247723d570ca8d7d5
erheinrichs <97911839+erheinrichs@users.noreply.github.com>
2022-02-21 23:31:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+	  "esri/widgets/Search",
+	  "esri/widgets/Legend",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Search, Legend, Home, PopupTemplate) {
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
+      
+      var scene = new WebScene({
+        portalItem:{
+         id:"6117ddab39e942cbb772cf8c8f2dd30c",
+          
+        }
       });
+
+      
+  
+      
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+		viewingMode:'global',
+      });
+	  
+        var homeBtn = new Home({
+        view: view
+      });
+      view.ui.add(homeBtn, "top-left");
+      
+	  var searchWidget = new Search({
+        view: view
+      });
+
+      
+      view.ui.add(searchWidget, {
+        position: "top-right"
+      });
+	  
+	    view.when(function() {
+         
+          var featureLayer = scene.layers.getItemAt(1);
+
+          var legend = new Legend({
+            view: view,
+            layerInfos: [{
+              layer: featureLayer,
+              title: "K-12 State Education Rankings"
+            }]
+          });
+        
+          view.ui.add(legend, "bottom-right");
+        });
+        
+    });
